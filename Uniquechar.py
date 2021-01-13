ab96781ff52617550a6ef97b0ecba36de9e31591
PardiAndrasGit <apardi@ibs-b.hu>
2021-01-13 18:59:44 +0100

    first commit

---
+++ Uniquechar.py
@@ -0,0 +1,11 @@
+uniquecharacters = []
+for x in 'anagram':
+    if not x in uniquecharacters:
+        uniquecharacters.append(x)
+print(uniquecharacters)
+
+uniquecharacters = []
+for x in 'codingisfun':
+    if not x in uniquecharacters:
+        uniquecharacters.append(x)
+print(uniquecharacters)
